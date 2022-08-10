0deb0fb7f7145f270df8b1008c52195a7975e8f4
xadmin <xadmin@bk.ru>
2022-08-10 12:02:09 +0300

    root

--- psyhobot.py
+++ psyhobot.py
@@ -1,3 +1,5 @@
+#version start
+
 import random
 import sqlite3
 import telebot
@@ -40,19 +42,6 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, reply_markup=kb_fruits, text=text_file.text_1, parse_mode="html")
     elif call.data == "score":
         print(call.message.chat.id)
-        bot.send_message(call.message.chat.id, """
-       <b>bold</b>, <strong>bold</strong>
-<i>italic</i>, <em>italic</em>
-<u>underline</u>, <ins>underline</ins>
-<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
-<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
-<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
-<a href="http://www.example.com/">inline URL</a>
-<a href="tg://user?id=91124946">inline mention of a user</a>
-<code>inline fixed-width code</code>
-<pre>pre-formatted fixed-width code block</pre>
-<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
-       """, parse_mode="html")
     elif call.data == "start_kurs":
         print(call.message.chat.id, call.data)
         video = open('static/video_1.mp4', 'rb')
